57e1a8dab060f94a0e39f05692deeac60ef29149
Kalyani <kalyaniambati7@gmail.com>
2023-12-04 22:46:18 +0530

    [styles]

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React, { useState } from "react";
 
-function App() {
+function Todoassignment() {
   const [allTodos, setTodos] = useState([]);
   const [newTitle, setNewTitle] = useState("");
   const [newDescription, setNewDescription] = useState("");
@@ -13,7 +13,7 @@ function App() {
       title: newTitle,
       description: newDescription,
       date: newDate,
-      priority: newPriority
+      priority: newPriority,
     };
 
     let updatedTodoArr = [...allTodos];
@@ -22,65 +22,79 @@ function App() {
   };
 
   const emptyit = (index) => {
-      let updatedTodoArr = [...allTodos];
-      let arr = updatedTodoArr.slice(0,index);
-      let arr1 = updatedTodoArr.slice(index+1,);
-      let arr2 = arr.concat(arr1);
-      setTodos(arr2);
-  }
-    
+    let updatedTodoArr = [...allTodos];
+    let initialtodolist = updatedTodoArr.slice(0, index);
+    let lasttodolist = updatedTodoArr.slice(index + 1);
+    let modifiedlist = initialtodolist.concat(lasttodolist);
+    setTodos(modifiedlist);
+  };
+
   return (
     <div>
-      <h1>Todo-List</h1>
-      <div className="main-content">
-        <label>Title</label>
-        <input
-          type="text"
-          value={newTitle}
-          onChange={(e) => setNewTitle(e.target.value)}
-          placeholder="write here"
-        />
+
+      <div>
+        <h1 className = "main-heading"> Todo-List </h1>
       </div>
 
-      <div className="main-content">
-        <label>Description</label>
-        <textarea
-          type="text"
-          placeholder="write here"
-          value={newDescription}
-          onChange={(e) => setNewDescription(e.target.value)}
-        />
-      </div>
+      <div className = "title-desc">
+        <div className = "main-content">
+          <label> Title </label>
+          <input
+            type = "text"
+            value = {newTitle}
+            onChange = {(e) => setNewTitle(e.target.value)}
+            placeholder = "write here"
+          />
+        </div>
 
-      <div className="main-content">
-        <label>Date</label>
-         <input type="date" 
-         value={newDate} 
-         onChange={(e) => setNewDate(e.target.value)} /> 
-      </div>
+        <div className = "main-content">
+          <label> Description </label>
+          <textarea
+            type = "text"
+            placeholder = "write here"
+            value = {newDescription}
+            onChange = {(e) => setNewDescription(e.target.value)}
+          />
+        </div>
 
-      <div className="main-content" onChange={(e) => setPriority(e.target.value)}>
-        <label>Priority</label>
-        <select>
-          <option value = "high"> high </option>
-          <option value = "medium"> medium </option>
-          <option value = "low"> low </option>
-        </select>
-      </div>
+        <div className = "main-content">
+          <label> Date </label>
+          <input
+            type = "date"
+            value = {newDate}
+            onChange = {(e) => setNewDate(e.target.value)}
+          />
+        </div>
 
-      <button className="first-button" onClick={handleAddTodo}>
-        Add
-      </button>
+        <div
+          className = "main-content"
+          onChange = {(e) => setPriority(e.target.value)}
+        >
+          <label> Priority </label>
+          <select>
+            <option value = "high"> high </option>
+            <option value = "medium"> medium </option>
+            <option value = "low"> low </option>
+          </select>
+        </div>
+
+        <button className = "first-button" onClick = {handleAddTodo}>
+          Add
+        </button>
+      </div>
 
       {allTodos.map(({ title, description, date, priority }, index) => {
         return (
-          <div key={index}>
-            <div>
-              <h3>{title}</h3>
-              <p>{description}</p>
-              <h3>{date}</h3>
-              <h3>{priority}</h3>
-              <button onClick={(e) => emptyit(index)}>delete</button>
+          <div key = {index}>
+            <div className = "todo">
+              <h3> {title} </h3>
+              <p> {description} </p>
+              <h3> {date} </h3>
+              <h3> {priority} </h3>
+              <button className = "todo-y" onClick = {(e) => emptyit(index)}>
+                x
+              </button>
+              <input type = "checkbox" />
             </div>
           </div>
         );
@@ -88,4 +102,4 @@ function App() {
     </div>
   );
 }
-export default App;
+export default Todoassignment;
